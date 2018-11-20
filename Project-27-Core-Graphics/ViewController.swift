be670315455b4fdaf745cfe13dc7b1247f554f91
David Verbenyi <dverbenyi@gmail.com>
2018-11-20 11:38:25 +0000

    Setup

--- Project-27-Core-Graphics/ViewController.swift
+++ Project-27-Core-Graphics/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    
+    @IBAction func redrawTapped(_ sender: UIButton) {
+    }
+    
 }
 
